7559f1339b17c509a31c137f633a545c60235282
Jason Glenn <glennwjason@gmail.com>
2019-01-19 13:58:41 -0500

    lighttest

---
+++ Together/Assets/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime, 0f, Input.GetAxis("Vertical") * Time.deltaTime);
+    }
+}
---
+++ Together/Assets/RayCast.cs
@@ -0,0 +1,37 @@
+﻿
+using UnityEngine;
+
+public class RayCast : MonoBehaviour
+{
+    public Camera lightRay;
+    public float lightRange = 7f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shoot();
+        
+    }
+
+    void shoot()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(lightRay.transform.position, lightRay.transform.forward, out hit, lightRange))
+        {
+            Debug.Log(hit.transform.name);
+            if (hit < lightRange)
+            {
+
+            }
+        }
+    }
+}
