dc1392d873bbd330c836304e8ca549e595953c43
Jason Glenn <glennwjason@gmail.com>
2019-01-20 11:31:04 -0500

    finalLighttest

--- Together/Assets/RayCast.cs
+++ Together/Assets/RayCast.cs
@@ -8,15 +8,16 @@ public class RayCast : MonoBehaviour
     //camera range for raycast
     public float lightRange = 7f;
     //reference the puzzle
-    public GameObject LightBox;
-    LightPuzzle lightPuzzle;
+    //public GameObject LightBox;
+   // LightPuzzle lightPuzzle;
+    enumLight hitByRay;
     
 
 
     // Start is called before the first frame update
     void Start()
     {
-        lightPuzzle = LightBox.GetComponent<LightPuzzle>();
+        //lightPuzzle = LightBox.GetComponent<LightPuzzle>();
         
     }
 
@@ -24,24 +25,38 @@ public class RayCast : MonoBehaviour
     void Update()
     {
         shoot();
-        
+       // if ((hitByRay == null) && (GetComponent<enumLight>() != null))
+       // {
+         //   hitByRay = GetComponent<enumLight>();
+          //  Debug.Log("got component");
+       // }
+       // else
+       // {
+         //   Debug.LogWarning("Missing hitByRay_script component. Please add one");
+       // }
+
     }
 
-    void shoot()
+   public void shoot()
     {
         RaycastHit hit;
 
         if (Physics.Raycast(transform.position, -Vector3.up, out hit))
         {
-            hit.transform.SendMessage("hitByRay");
+           // hit.transform.SendMessage("hitByRay");
         }
 
         if (Physics.Raycast(lightRay.transform.position, lightRay.transform.forward, out hit, lightRange))
         {
-           Debug.Log(hit.transform.name);
+            //Debug.Log(hit.transform.name);
+            Debug.DrawRay(lightRay.transform.position, lightRay.transform.forward);
+
+            hitByRay = hit.collider.GetComponent<enumLight>();
             if (hit.distance < lightRange)
             {
-
+                Debug.Log(hit.transform.name);
+                //hit.collider.gameObject.GetComponent<enumLight>().HitByray();
+                hitByRay.startLerp();
             }
         }
     }
--- Together/Assets/enumLight.cs
+++ Together/Assets/enumLight.cs
@@ -8,26 +8,43 @@ public class enumLight : MonoBehaviour
     public Color startColor;
     public Color endColor;
     float startTime;
+    bool isfading;
     // Start is called before the first frame update
+    Renderer rend;
+    public Color invisible = Color.white;
     
     void Start()
     {
-        startTime = Time.time;
+        rend = GetComponent<Renderer>();
+        invisible.a = 0.0f;
+        rend.material.color = invisible;
     }
 
     // Update is called once per frame
     void Update()
     {
-        RaycastHit hit;
-        float t = (Time.time - startTime) * speed;
-        GetComponent<Renderer>().material.color = Color.Lerp(startColor, endColor, t);
+
         //Debug.Log(Color.Lerp(startColor, endColor, t));
         //Debug.Log(hit.transform.name);
-        
-        //hitByray();
+        if (isfading)
+        {
+            HitByray();
+        }
     }
-    void hitByray()
+    public void HitByray()
     {
         Debug.Log("I was hit");
+        //RaycastHit hit;
+        float t = (Time.time - startTime) * speed;
+        GetComponent<Renderer>().material.color = Color.Lerp(startColor, endColor, t);
+    }
+
+    public void startLerp()
+    {
+        if (isfading == false)
+        {
+            isfading = true;
+            startTime = Time.time;
+        }
     }
 }
